28cb3724381c7b2e8bcf038083f5a1e507c64137
pengmeng <mengp3157@gmail.com>
2015-03-05 17:37:05 -0600

    rewrite scraper

--- recipe/crawler/scraper.py
+++ recipe/crawler/scraper.py
@@ -6,52 +6,57 @@ from handler import RecipeHandler
 
 class Scraper(object):
 
-    def __init__(self, urls, handler=None):
-        self._urls = urls
-        self._handler = handler
+    def __init__(self, debug=False):
+        self.debug = debug
 
-    @property
-    def urls(self):
-        return self._urls
+    def fetchone(self, url, handler):
+        result = self.fetch([url], handler)
+        return result[url]
 
-    @urls.setter
-    def urls(self, value):
-        self._urls = value
-
-    @property
-    def handler(self):
-        return self._handler
-
-    @handler.setter
-    def handler(self, value):
-        self._handler = value
-
-    def fetch(self):
+    def fetch(self, urllist, handler):
         result = {}
-        for url in self._urls:
-            html = urllib.urlopen(url).read()
-            self._save2file(url, html)
-            result[url] = self._handler.parse(html, url)
+        for url in urllist:
+            filename = self._tmpfilename(url)
+            if os.path.exists(filename):
+                html = self._loodfile(filename)
+                if self.debug:
+                    print('Load {0} from file.'.format(filename))
+            else:
+                html = urllib.urlopen(url).read()
+                self._save2file(filename, html)
+                if self.debug:
+                    print('Download {0} and save as {1}'.format(url, filename))
+            result[url] = handler.parse(html, url)
         return result
 
-    def _save2file(self, url, content):
-        if not os.path.exists('./tmp'):
-            os.mkdir('./tmp')
-        filename = "./tmp/" + str(hash(url) & 0xffffffff) + ".html"
+    def _loodfile(self, filename):
+        with open(filename, 'r') as infile:
+            content = infile.read()
+        return content
+
+    def _save2file(self, filename, content):
         with open(filename, 'w') as outfile:
             outfile.write(content)
             outfile.flush()
 
+    def _tmpfilename(self, url):
+        if not os.path.exists('./tmp'):
+            os.mkdir('./tmp')
+        return './tmp/' + str(hash(url) & 0xffffffff) + '.html'
+
 if __name__ == '__main__':
-    urls = ["http://allrecipes.com/Recipe/Baked-Coconut-Shrimp/Detail.aspx",
+    urls = ['http://allrecipes.com/Recipe/Baked-Coconut-Shrimp/Detail.aspx',
             'http://allrecipes.com/Recipe/Pork-Chops-with-Apple-Cider-Glaze/Detail.aspx',
             'http://allrecipes.com/Recipe/Butter-Roasted-Cauliflower/Detail.aspx',
             'http://allrecipes.com/Recipe/Zucchini-Yogurt-Multigrain-Muffins/Detail.aspx',
             'http://allrecipes.com/Recipe/Easy-Corned-Beef-and-Cabbage/Detail.aspx']
     hd = RecipeHandler()
-    sp = Scraper(urls, hd)
-    r = sp.fetch()
-    print(r[urls[4]].time)
+    sp = Scraper(True)
+    r1 = sp.fetch(urls, hd)
+    print(len(r1))
+    r2 = sp.fetchone('http://allrecipes.com/Recipe/Amish-Meatloaf/Detail.aspx', hd)
+    print(r2)
+    # print(r[urls[4]].time)
     # print(r[urls[1]].ing)
     # print(r[urls[2]].ing)
     # print(r[urls[3]].ing)

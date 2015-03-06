be821d58b5d7f4fb76c74aadaef8cd983aa3bd3c
pengmeng <mengp3157@gmail.com>
2015-03-06 16:15:22 -0600

    move marisa

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 __author__ = 'mengpeng'
 from transformer.crawler.scraper import Scraper
 from transformer.crawler.handler import RecipeHandler
+from transformer.trie import Trie
 
 
 if __name__ == '__main__':
@@ -11,5 +12,20 @@ if __name__ == '__main__':
             'http://allrecipes.com/Recipe/Easy-Corned-Beef-and-Cabbage/Detail.aspx']
     hd = RecipeHandler()
     sp = Scraper(True)
-    recipes = sp.fetch(urls, hd)
-    print(len(recipes))
+    result = sp.fetch(urls, hd)
+    #results contains recipe objects
+    #get each recipe by their url
+    recipe = result[urls[0]]
+    print(recipe.name)
+
+    #Follwing example show how to get knowledge base
+    #all knowledge base are in transformer/marisa/
+    actions = Trie.getTrieByName('actions')
+    #try to find whether an action is in list
+    if 'bake' in actions:
+        #get all match items with prefix 'bake'
+        print(actions.byPrefix('bake'))
+    #get the list of proteins
+    proteins = Trie.getTrieByName('proteins')
+    #get all item with prefix 'European'
+    print(proteins.byPrefix('European'))

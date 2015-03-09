e7507157b747621b90f5f9ef9bcfac820643eb6d
pengmeng <mengp3157@gmail.com>
2015-03-09 01:03:07 -0500

    add converter.py

---
+++ transformer/converter.py
@@ -0,0 +1,11 @@
+__author__ = 'mengpeng'
+
+
+class Converter(object):
+
+    def __init__(self, recipe):
+        self.oldrecipe = recipe
+        self.newrecipe = None
+
+    def convertTo(self, keyword):
+        pass

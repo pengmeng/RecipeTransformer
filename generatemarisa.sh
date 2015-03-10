c5a69e426f8f6537bf0901ad4d3bf6beb60d1959
pengmeng <mengp3157@gmail.com>
2015-03-09 21:22:19 -0500

    update script and marisa models

--- generatemarisa.sh
+++ generatemarisa.sh
@@ -1,3 +1,5 @@
 python transformer/csv2marisa.py csv/tools.csv csv/fruits.csv csv/vegetables.csv csv/proteins.csv csv/actions.csv csv/descriptors.csv csv/preparation.csv
 
+python transformer/csv2marisa.py bytes csv/lactose-free.csv
+
 cp -R ./marisa ./test

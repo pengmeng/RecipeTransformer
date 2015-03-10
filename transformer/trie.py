89799a9401a599d2531ad3f9332eb8f9caaa2b59
pengmeng <mengp3157@gmail.com>
2015-03-09 21:12:36 -0500

    update trie to support BytesTrie

--- transformer/trie.py
+++ transformer/trie.py
@@ -12,13 +12,13 @@ class Trie(object):
         self.name = name
 
     @staticmethod
-    def getTrieByName(name):
+    def getTrieByName(name, bytestrie=False):
         if name in Trie.TrieDict:
             return Trie.TrieDict[name]
         filename = './marisa/' + name + '.marisa'
         if not os.path.exists(filename):
             raise AttributeError('{0} is not found in marisa models.'.format(filename))
-        trie = marisa_trie.Trie()
+        trie = marisa_trie.Trie() if not bytestrie else marisa_trie.BytesTrie()
         trie.load(filename)
         mytrie = Trie(trie, name)
         Trie.TrieDict[name] = mytrie
@@ -37,3 +37,6 @@ class Trie(object):
     def byPrefix(self, prefix):
         result = self.trie.keys(unicode(prefix.lower()))
         return map(str, result)
+
+    def items(self):
+        return iter(self.trie.items())

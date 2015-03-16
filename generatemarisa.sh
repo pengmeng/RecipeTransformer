d83f9b42905ce485f6cd00642e20cb26d8c96236
pengmeng <mengp3157@gmail.com>
2015-03-16 18:55:01 -0500

    clean code

--- generatemarisa.sh
+++ generatemarisa.sh
@@ -1,4 +1,4 @@
-python transformer/csv2marisa.py csv/tools.csv csv/fruits.csv csv/vegetables.csv csv/proteins.csv csv/actions.csv csv/descriptors.csv csv/preparation.csv
+python transformer/csv2marisa.py csv/tools.csv csv/proteins.csv csv/actions.csv csv/descriptors.csv csv/preparation.csv
 
 python transformer/csv2marisa.py bytes csv/lactose-free.csv csv/low-fat.csv csv/low-calorie.csv csv/low-sodium.csv csv/vegan.csv
 
